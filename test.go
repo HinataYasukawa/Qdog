0f587fe18a26cbaafcc8c10a99a9605193f6b90b
HinataYasukawa <h1i2n2a62208@gmail.com>
2024-10-17 14:28:10 +0900

    random関数を追加

--- test.go
+++ test.go
@@ -3,8 +3,10 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 // 図形の値を定義
@@ -13,6 +15,12 @@ var shapeValues = map[string]int{
 	"△": 3,
 	"□": 4,
 	"☆": 5,
+}
+
+func random()(int){
+	rand.Seed(time.Now().UnixNano())
+	result := rand.Intn(1)
+	return result
 }
 
 // 問題を生成
@@ -31,7 +39,7 @@ func main() {
 	fmt.Printf("問題: %s %s\n", shape1, shape2)
 	fmt.Println("回答を選択してください:")
 	fmt.Println("q: !")
-	fmt.Println("w: 数字")
+	fmt.Println("w:" ,correctSum)
 	fmt.Println("e: E")
 
 	// ユーザーの回答を取得
@@ -42,11 +50,11 @@ func main() {
 
 	// 正解の判定
 	if input == "w" && correctSum == shapeValues[shape1]+shapeValues[shape2] {
-		fmt.Println("正解！数字です。")
+		fmt.Println("正解。", correctSum, "です。")
 	} else if input == "e" && correctSum != shapeValues[shape1]+shapeValues[shape2] {
-		fmt.Println("正解！Eです。")
+		fmt.Println("正解。Eです。")
 	} else if input == "q" {
-		fmt.Println("正解！Qです。")
+		fmt.Println("正解。!です。")
 	} else {
 		fmt.Println("不正解です。")
 	}
